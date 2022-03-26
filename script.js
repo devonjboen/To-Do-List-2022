5921561c24cfa979afebfae725442ce0092d66e8
Devon Boen <devonjboen@gmail.com>
2022-03-26 17:10:54 -0600

    Implemented checkmark and delete buttons

---
+++ script.js
@@ -0,0 +1,29 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+//Prevent form from submitting
+event.preventDefault();
+ //Todo DIV
+ const todoDiv = document.createElement('div')
+ todoDiv.classList.add("todo")
+ //Create LI
+ const newTodo = document.createElement('li');
+ newTodo.innterText = 'hey'
+ newTodo.classList.add('todo-item')
+ todoDiv.appendChild(newTodo);
+ //CHECKMARK BUTTON
+ const completedButton = document.createElement('button');
+ completedButton.innerHTML = '<i class="fa-solid fa-check-double"></i>';
+ completedButton.classList.add('complete-btn');
+ todoDiv.appendChild(completedButton);
+ //DELETE BUTTON
+ const deleteButton = document.createElement('button');
+ deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+ deleteButton.classList.add('delete-btn');
+ todoDiv.appendChild(deleteButton);
+ todoList.appendChild(todoDiv);
+}
